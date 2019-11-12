ca38affc242e909b705a95847c70ba15b634bca0
Aibek <aibek.bekbayev@struktur.de>
2019-11-12 19:29:02 +0100

    refactor

--- pattern_match.py
+++ pattern_match.py
@@ -2,88 +2,148 @@ import os
 import sys
 from typing import (
     List, Tuple, Any,
-    Generic, TypeVar, Callable
+    Generic, TypeVar, Callable, GenericMeta
 )
 import inspect
 
-global_functions_dic = {}
+from collections import OrderedDict
+from inspect import Parameter
 
-def match(s, pattern):
-    args = s["args"]
-    kwargs = s["kwargs"]
-    params_dict = {}
-    if not len(args) + len(kwargs) == len(pattern):
+class EmptyDefaultValue:
+    pass
+
+class NoMatchError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class MultipleMatchError(Exception):
+    def __init__(self, msg):
+        super().__init__(msg)
+
+class PatternMapper:
+    def __init__(self):
+        self.func_dict = {}
+    def __getitem__(self, key: str)->OrderedDict:
+        return self.func_dict[key]
+    def __setitem__(self, key: str, value: OrderedDict)->None:
+        self.func_dict[key] = value
+    def add_pattern(self, name: str, pattern: 'Pattern', func):
+        if not name in self.func_dict:
+            self[name] = []
+        self[name].append({"func": func, "pattern": pattern})
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(PatternMapper, cls).__new__(cls)
+        return cls.instance
+    @staticmethod
+    def get_name(func):
+        return func.__code__.co_name
+    @staticmethod
+    def get_params(func):
+        s = inspect.signature(func)
+        return OrderedDict([(param, (s.parameters[param].default, s.parameters[param].annotation)) for param in s.parameters])
+
+class PatternTerm(object):
+    def __init__(self, name: str, value: Any, annotation: Any):
+        self.name = PatternTerm.process_name(name)
+        self.value = PatternTerm.process_value(value)
+        self.annotation = PatternTerm.process_annotation(annotation)
+    @staticmethod
+    def process_annotation(annotation: Any):
+        if annotation is inspect._empty:
+            return Any
+        if PatternTerm.is_generic(annotation):
+            return PatternTerm.process_generic(annotation)
+        if PatternTerm.is_class(annotation):
+            return annotation
+        if PatternTerm.is_type(annotation):
+            return annotation
+        if annotation is Any:
+            return annotation
+    @staticmethod
+    def process_value(value: Any):
+        if value is inspect._empty:
+            return EmptyDefaultValue()
+        else:
+            return value
+    @staticmethod
+    def process_name(name: str)->str:
+        return name
+    @staticmethod
+    def is_generic(annotation: Any)->bool:
+        return isinstance(annotation, GenericMeta)
+    @staticmethod
+    def is_class(annotaion: Any)->bool:
+        return isinstance(annotaion.__class__,  type)
+    @staticmethod
+    def is_type(annotaion: Any)->bool:
+        return isinstance(annotation, type)
+
+    def __eq__(self, pattern_term: 'PatternTerm')->bool:
+        return self.check_name(pattern_term.name) and self.check_annotation(pattern_term.annotation) and self.check_value(pattern_term.value)
+    def check_name(self, name: str)->bool:
+        return self.name == "" or self.name == name
+    def check_value(self, value: Any)->bool:
+        return isinstance(value, EmptyDefaultValue)or isinstance(self.value, EmptyDefaultValue) or self.value == value
+    def check_annotation(self, annotation: Any):
+        if annotation is Any:
+            return True
+        if PatternTerm.is_generic(annotation):
+           return self.match_generic(annotaion)
+        if PatternTerm.is_class(annotation):
+            return self.annotation is annotation
+        if PatternTerm.is_type(annotation):
+            return self.annotation is annotaion
+    def match_generic(self, annotation):
         return False
-    for i, param in enumerate(pattern.values()):
-        annotation = param.annotation
-        if annotation is param.empty:
-            annotation = Any
-        default = param.default
-        if default is param.empty:
-            default = None
-        name = param.name
-        if name is param.empty:
-            name = i
-        params_dict[name] = [default, annotation]
-    is_type_matches = True
-    is_value_matches = True
-    for s_arg, p_arg in zip(args, list(params_dict.values())[:len(args)]):
-       if p_arg[1] is Any:
-            is_type_matches = is_type_matches and True
-       else:
-            is_type_matches = is_type_matches and type(s_arg) is p_arg[1]
-       if p_arg[0] is None:
-            is_value_matches = is_value_matches and True
-       elif is_type_matches:
-            is_value_matches = is_value_matches and p_arg[0] == s_arg
-       else:
-            is_value_matches = False
+    def __str__(self):
+        return "({0}, {1}, {2})".format(self.name, self.value, self.annotation)
 
-    for k_arg in kwargs:
-        if not k_arg in params_dict:
-            return False
-        if params_dict[k_arg][1] is Any:
-            is_type_matches = is_type_matches and True
+
+class Pattern(object):
+    def __init__(self, *args, **kwargs):
+        processed_args = [("", arg, type(arg)) for arg in args] + [(key, kwargs[key][0], kwargs[key][1]) for key in kwargs]
+        self.pattern_terms = [PatternTerm(*arg) for arg in processed_args]
+    def __eq__(self, pattern: 'Pattern'):
+        return self.pattern_terms == pattern.pattern_terms
+    def __str__(self):
+        return "<Pattern: ["+",".join([str(term) for term in self.pattern_terms])+"]>"
+    def __len__(self):
+        return len(self.pattern_terms)
+
+pattern_mapper = PatternMapper()
+def match_parameters(name: str): 
+    def __match_parameters( *args, **kwargs):
+        processed_kwargs = {key: (kwargs[key], type(kwargs[key])) for key in kwargs}
+        curr_pattern = Pattern(*args, **processed_kwargs)
+        patterns = pattern_mapper[name]
+        matched = list(filter(lambda x: curr_pattern == x["pattern"], patterns))
+        if False and len(matched)>1:
+            raise MultipleMatchError("{0} corresponds to {1} matches".format(curr_pattern, len(matched)))
+        elif not len(matched):
+            raise NoMatchError("{0} mathches with no pattern".format(curr_pattern))
         else:
-            is_type_matches = is_type_matches and type(kwargs[k_arg]) is params_dict[k_arg][1]
+            return matched[0]["func"](*args, **kwargs)
+    return __match_parameters
 
-        if params_dict[k_arg][0] is None:
-            is_value_matches = is_value_matches and True
-        elif is_type_matches:
-            is_value_matches = is_value_matches and params_dict[k_arg][0] == kwargs[k_arg]
-        else:
-            is_value_matches = False
-    return is_value_matches and is_type_matches
-
-def pm(a):
-    if inspect.isfunction(a):
-       sig = inspect.signature(a)
-       func_name = a.__code__.co_name
-       if func_name in global_functions_dic:
-           global_functions_dic[func_name].append((a, sig.parameters))
-       else:
-           global_functions_dic[func_name] = [(a, sig.parameters)]
-       def preprocess(*args, **kwargs):
-           curr_args = {"args": args, "kwargs": kwargs}
-           for params in global_functions_dic[func_name]:
-               if match(curr_args, params[1]):
-                   return params[0](*args, **kwargs)
-           raise TypeError("No pattern is found")
-       return preprocess
-
-
+def pm(func):
+    name = PatternMapper.get_name(func)
+    pattern = Pattern(**PatternMapper.get_params(func))
+    pattern_mapper.add_pattern(name, pattern, func)
+    return match_parameters(name)
 
 
 
 @pm
-def f(n=0):
-    return 1
-@pm
-def f(n=1):
+def f(n:int=1)->int:
     return 1
 
 @pm
-def f(n: int)->int:
-    return f(n-1) + f(n-2)
+def f(n:int=2):
+    return 1
 
-print(f(10))
+@pm
+def f(n: int):
+    return f(n-1)+f(n-2)
+
+print(f(11))
